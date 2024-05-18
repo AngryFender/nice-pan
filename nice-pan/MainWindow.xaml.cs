2023f992b5470be4c25342af681cb8cd575e6a55
AngryFender <teaBiscuitPerson@gmail.com>
2024-05-18 23:51:38 +0100

    Github issue #2: Added checks to ensure only one instance of the apps runs at a time

--- nice-pan/MainWindow.xaml.cs
+++ nice-pan/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace nice_pan
 {
@@ -16,9 +10,27 @@ namespace nice_pan
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _exitRequested = false;
+        private bool _initialNotification = true;
+        private readonly Mutex _mutex;
+
         public MainWindow()
         {
-            InitializeComponent();
+            bool isNewApp = true;
+            _mutex = new Mutex(true, "Peeky Blinkers", out isNewApp);
+
+            if (!isNewApp)
+            {
+                ErrorDialog dialog = new ErrorDialog();
+                dialog.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                InitializeComponent();
+
+            }
         }
+
     }
 }
